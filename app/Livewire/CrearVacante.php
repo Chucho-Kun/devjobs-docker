e017f67aad5d4cc9400356e744094b17652e812c
Jesus Peralta <jesus_web_master@hotmail.com>
2025-10-07 13:45:32 -0600

    saving in databases

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,10 +4,58 @@ namespace App\Livewire;
 
 use App\Models\Categorias;
 use App\Models\Salario;
+use App\Models\Vacante;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class CrearVacante extends Component
 {
+
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required|image|max:1024'
+    ];
+
+    public function crearVacante()
+    {
+        $datos = $this->validate();    
+
+        // Almacenar la Imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $nombre_imagen = str_replace('public/vacantes/' , '' , $imagen);
+        
+        // Crear la vacante
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'],
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $nombre_imagen,
+            'user_id' => Auth::user()->id
+        ]);
+        // Crear un mensaje
+
+        // Redireccionar 
+    }
+
     public function render()
     {
         // Consultar DB desde el Modelo Salario
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,15 +1,15 @@
-<form action="" class="md:w-1/2 space-y-5">
-    @csrf
+<form action="" class="md:w-1/2 space-y-5" wire:submit.prevent='crearVacante'>
     <div>
         <x-input-label for="titulo" :value="__('Titulo Vacante')" />
-        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')"
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')"
             autocomplete="titulo" placeholder="Titulo Vacante" />
-        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+        <x-input-error :messages="$errors->get('titulo')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
+        
     </div>
 
     <div>
         <x-input-label for="salario" :value="__('Salario Mensual')" />
-        <select name="salario" id="salario"
+        <select wire:model="salario" id="salario"
             class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full"
         >
         <option>-- Seleccione --</option>
@@ -17,31 +17,37 @@
             <option value="{{ $salario->id }}">{{ $salario->salario }}</option>
         @endforeach
         </select>
+        <x-input-error :messages="$errors->get('salario')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
     </div>
 
     <div>
-        <x-input-label for="salario" :value="__('Categoria')" />
-        <select name="categoria" id="categoria"
+        <x-input-label for="categoria" :value="__('Categoria')" />
+        <select wire:model="categoria" id="categoria"
             class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
         <option>-- Seleccione --</option>
         @foreach ( $categorias as $categoria )
             <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
         @endforeach
         </select>
+        <x-input-error :messages="$errors->get('categoria')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
     </div>
+
     <div>
         <x-input-label for="empresa" :value="__('Empresa')" />
-        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" autocomplete="empresa" placeholder="Nombre de la empresa" />
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" autocomplete="empresa" placeholder="Nombre de la empresa" />
     </div>
-    <div>
-        <x-input-label for="ultimo_dia" :value="__('Fecha Límite de Postulación')" />
-        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')" autocomplete="ultimo_dia" />
-    </div>
+    <x-input-error :messages="$errors->get('empresa')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
     
     <div>
-        <x-input-label for="ultimo_dia" :value="__('Descripción del Puesto')" />
+        <x-input-label for="ultimo_dia" :value="__('Fecha Límite de Postulación')" />
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')" autocomplete="ultimo_dia" />
+    </div>
+    <x-input-error :messages="$errors->get('ultimo_dia')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción del Puesto')" />
         <textarea 
-            name="descripcion" 
+            wire:model="descripcion" 
             placeholder="Descripción general del puesto" 
             id="" 
             cols="30" 
@@ -51,11 +57,21 @@
 
         </textarea>
     </div>
+    <x-input-error :messages="$errors->get('descripcion')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
 
     <div>
         <x-input-label for="imagen" :value="__('Imagen')" />
-        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="imagen" />
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen" accept="image/*" />
     </div>
+
+    <div class="my-5 w-80">
+        @if ($imagen)
+            Imagen:
+            <img src="{{ $imagen->temporaryUrl() }}" alt="preview de imagen">
+        @endif
+    </div>
+
+    <x-input-error :messages="$errors->get('imagen')" class="border border-red-700 bg-red-100 text-red-600 font-bold uppercase p-2 mt-2 text-xs" />
 
     <x-primary-button>Crear Vacante</x-primary-button>
 
